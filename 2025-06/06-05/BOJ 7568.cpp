a2b8871767a2b8e7c9767ee901926be925d2a549
dahyun_0_1 <changum0520@gmail.com>
2025-06-05 17:26:59 +0900

    [BOJ 7568] 덩치 문제풀이

---
+++ 2025-06/06-05/BOJ 7568.cpp
@@ -0,0 +1,38 @@
+// 덩치
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+
+	int n;
+	cin >> n;
+	vector<pair<int, int>> v(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> v[i].first >> v[i].second;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		int rank = 1;
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j) continue;
+			if (v[i].first < v[j].first && v[i].second < v[j].second)
+			{
+				rank++;
+			}
+		}
+		cout << rank << ' ';
+	}
+
+
+	return 0;
+}
