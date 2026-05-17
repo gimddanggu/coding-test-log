5327128c2b7b1760082af3d646137c8465567ec4
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2026-05-17 12:51:03 +0900

    [Passed] 10개 입력받기 (8ms, 0MB)

---
+++ CodeTree/trail1/10개 입력받기/receive-10-inputs.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[10] = {};
+    int cnt = 0;
+    double sum_a = 0; 
+
+    for (int i=0; i < 10; i++) {
+        cin >> arr[i];
+        if (arr[i] == 0) break;
+        sum_a += arr[i];
+        cnt++;
+    }
+    cout << sum_a << " ";
+    cout << fixed;
+    cout.precision(1);
+    cout << sum_a / cnt;
+    return 0;
+}
