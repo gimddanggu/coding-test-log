11ad8f246e6bf0c8c3fb1475070ab426e97018d6
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2026-05-21 14:11:55 +0900

    [Passed] 가장 큰 수 고르기 (2ms, 0MB)

--- CodeTree/trail1/가장 큰 수 고르기/picking-biggest-number.cpp
+++ CodeTree/trail1/가장 큰 수 고르기/picking-biggest-number.cpp
@@ -2,17 +2,23 @@
 #include <climits>
 using namespace std;
 
+int max_func (int arr[]) {
+    int max_val = INT_MIN;
+    for (int i = 0; i <  10; i++) {
+        if (max_val < arr[i]) max_val = arr[i];
+    }
+
+    return max_val;
+}
+
 int main() {
     // Please write your code here.
     int arr[10] = {};
-    int max_val = INT_MIN;
     for (int i=0; i < 10; i++) {
         cin >> arr[i];
-
-        if (max_val < arr[i]) max_val = arr[i];
     }
     
-    cout << max_val;
+    cout << max_func(arr);
     return 0;
 
 }
