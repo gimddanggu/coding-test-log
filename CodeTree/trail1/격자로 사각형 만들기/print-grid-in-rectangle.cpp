d9ce5bee9e3cff2422b71443fe1b6822173a04d5
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2026-05-27 01:47:07 +0900

    [Passed] 격자로 사각형 만들기 (2ms, 0MB)

---
+++ CodeTree/trail1/격자로 사각형 만들기/print-grid-in-rectangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    int arr[10][10] = {};
+    cin >> n;
+
+    for (int i=0; i < n; i++) {
+        arr[0][i] = 1;
+        arr[i][0] = 1;
+    }
+
+    for (int i=1; i < n; i++) {
+        for (int j=1; j < n; j++) {
+            arr[i][j] = arr[i][j-1] + arr[i-1][j-1] + arr[i-1][j];
+        }
+    }
+
+    for (int i=0; i < n; i++) {
+        for (int j=0; j < n; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
