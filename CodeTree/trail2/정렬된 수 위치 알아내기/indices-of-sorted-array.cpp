76dbc19401708d47c0bdf6b133bf669d367578d0
dahyun_0_1 <107992381+gimddanggu@users.noreply.github.com>
2026-06-11 12:44:51 +0900

    [Passed] 정렬된 수 위치 알아내기 (2ms, 0MB)

--- CodeTree/trail2/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
+++ CodeTree/trail2/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define MAX 1000
 
 pair<int,int> arr[MAX] = {};
-
+int ans[MAX] = {};
 int main() {
     // Please write your code here.
     int n;
@@ -19,15 +19,23 @@ int main() {
 
     sort(arr, arr+n);
 
+    // for (int i=0; i<n; i++) {
+    //     int cnt=1;
+    //     for (int j=0; j < n; j++) {
+    //         if (i == arr[j].second) { 
+    //             cout << cnt << " ";
+    //             break;
+    //         }
+    //         cnt ++;
+    //     }
+    // }
+
     for (int i=0; i<n; i++) {
-        int cnt=1;
-        for (int j=0; j < n; j++) {
-            if (i == arr[j].second) { 
-                cout << cnt << " ";
-                break;
-            }
-            cnt ++;
-        }
+        ans[arr[i].second] = i+1;
+    }
+
+    for (int i=0; i<n; i++) {
+        cout << ans[i] << " ";
     }
     return 0;
 }
